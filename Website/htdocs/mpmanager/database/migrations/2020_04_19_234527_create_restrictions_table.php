693f36ad1f8e10afba4f4ac3c43eb2514971a677
Kemal <alchalade@gmail.com>
2020-04-21 03:30:12 +0300

    create database tables for limiting / upgrading
    create models for restriction and upgrade
    
    add data_stream property

---
+++ Website/htdocs/mpmanager/database/migrations/2020_04_19_234527_create_restrictions_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestrictionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restrictions', static function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('target');
+            $table->integer('default');
+            $table->integer('limit');
+            $table->timestamps();
+        });
+
+        $this->addDefault();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restrictions');
+    }
+
+    public function addDefault()
+    {
+        DB::table('restrictions')->insert([
+            'target' => 'enable-data-stream',
+            'default' => 5,
+            'limit' => 5
+        ]);
+        DB::table('restrictions')->insert([
+            'target' => 'maintenance-user',
+            'default' => 5,
+            'limit' => 5
+        ]);
+
+    }
+}
