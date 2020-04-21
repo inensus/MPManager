f8a7a7e6bac93e10aa6c25ebd240fc0892fa8b0c
Kemal <alchalade@gmail.com>
2020-04-21 03:30:12 +0300

    create database tables for limiting / upgrading
    create models for restriction and upgrade
    
    add data_stream property

---
+++ Website/htdocs/mpmanager/app/Models/Upgrade.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class Upgrades
+ * @package App\Models
+ *
+ * @property  int $id
+ * @property int $restriction_id
+ * @property int $cost
+ * @property int $period_in_months
+ */
+class Upgrade extends Model
+{
+
+    public function restriction(): BelongsTo
+    {
+        return $this->belongsTo(Restriction::class);
+    }
+}
