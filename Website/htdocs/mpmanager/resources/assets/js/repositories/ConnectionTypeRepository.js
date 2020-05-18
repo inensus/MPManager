1886f832c1038dc0e49ba7271ada7b2c8cdb775a
93Kamuran <enginkamuran@gmail.com>
2020-05-18 18:37:00 +0300

    Connection Type repository and service added

---
+++ Website/htdocs/mpmanager/resources/assets/js/repositories/ConnectionTypeRepository.js
@@ -0,0 +1,12 @@
+const resource = '/api/connection-types'
+
+export default {
+    list() {
+        return axios.get(`${resource}`)
+    },
+    create(name) {
+        return axios.post(`${resource}`, name)
+    }
+}
+
+
