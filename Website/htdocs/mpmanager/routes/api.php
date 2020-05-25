ca168d908d2c6dc0caff301bde84a25c6d178179
Kemal <alchalade@gmail.com>
2020-05-25 11:27:11 +0300

    add restrictions

--- Website/htdocs/mpmanager/routes/api.php
+++ Website/htdocs/mpmanager/routes/api.php
@@ -91,7 +91,7 @@ Route::group(['middleware' => 'jwt.verify', 'prefix' => 'tariffs'], function () 
 //JWT authentication
 Route::group([
     'middleware' => 'api',
-    'prefix'     => 'auth'
+    'prefix' => 'auth'
 
 ], static function ($router) {
 
@@ -177,16 +177,16 @@ Route::post('androidApp', function (AndroidAppRequest $r) {
         DB::beginTransaction();
 
         //check if the meter id or the phone already exists
-        $meter  = Meter::where('serial_number', $r->get('serial_number'))->first();
+        $meter = Meter::where('serial_number', $r->get('serial_number'))->first();
         $person = null;
 
         if ($meter === null) {
-            $meter          = new Meter();
+            $meter = new Meter();
             $meterParameter = new MeterParameter();
-            $geoLocation    = new GeographicalInformation();
+            $geoLocation = new GeographicalInformation();
         } else {
             $meterParameter = MeterParameter::where('meter_id', $meter->id)->first();
-            $geoLocation    = $meterParameter->geo()->first();
+            $geoLocation = $meterParameter->geo()->first();
             if ($geoLocation === null) {
                 $geoLocation = new GeographicalInformation();
             }
@@ -199,7 +199,7 @@ Route::post('androidApp', function (AndroidAppRequest $r) {
 
         if ($person === null) {
             $personService = new PersonService(new App\Models\Person\Person());
-            $person        = $personService->createFromRequest($r);
+            $person = $personService->createFromRequest($r);
         }
 
         $meter->serial_number = $r->get('serial_number');
@@ -303,3 +303,5 @@ Route::group(['prefix' => '/maintenance'], function () {
     Route::post('/user', 'MaintenanceUserController@store')
         ->middleware('restriction:maintenance-user');
 });
+
+Route::post('/restrictions', 'RestrictionController@store');
