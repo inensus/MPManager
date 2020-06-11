f3d9c5f7dd74d583b2bf1f0d8400f82055bb91a7
Kemal <alchalade@gmail.com>
2020-06-11 20:17:15 +0300

    rename to ServicesServiceProvider

---
+++ Website/htdocs/mpmanager/app/Providers/ServicesServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\IUserService;
+use App\Services\IWeatherDataProvider;
+use App\Services\OpenWeatherMap;
+use App\Services\UserService;
+use Illuminate\Support\ServiceProvider;
+
+class ServicesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        // Bind UserService to IUserService
+        $this->app->bind(IUserService::class, UserService::class);
+
+        //Bind open weather map service   to IWeatherDataProvider interface
+        $this->app->bind(IWeatherDataProvider::class, OpenWeatherMap::class);
+    }
+}
