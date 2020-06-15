63c8d7ed67ea69e015ea7aa52dfd20477efc2f0a
93Kamuran <enginkamuran@gmail.com>
2020-06-15 14:13:56 +0300

    City service refactored.

--- Website/htdocs/mpmanager/resources/assets/js/services/CityService.js
+++ Website/htdocs/mpmanager/resources/assets/js/services/CityService.js
@@ -1,50 +1,53 @@
 import Repository from '../repositories/RepositoryFactory'
-import {ErrorHandler} from "../Helpers/ErrorHander";
+import { ErrorHandler } from '../Helpers/ErrorHander'
 
 export class CityService {
-    constructor() {
-        this.repository = Repository.get('city');
+    constructor () {
+        this.repository = Repository.get('city')
         this.cities = []
-        this.cities = [];
+        this.cities = []
         this.city = {
             id: 0,
-            name : "",
-            cluster_id : 0,
-            mini_grid_id : 0,
+            name: '',
+            cluster_id: 0,
+            mini_grid_id: 0,
         }
     }
 
-    async getCities() {
+    async getCities () {
         try {
-            let response = await this.repository.list();
+            let response = await this.repository.list()
             if (response.status === 200 || response.status === 201) {
-                this.cities = response.data.data;
-                return this.cities;
-            }else {
-                return new ErrorHandler(response.error, 'http', response.status);
-            }
-        }catch (e) {
-            return new ErrorHandler(e, 'http');
-        }
-    }
-    async createCity() {
-        let city_PM = {
-            'name': this.city.name,
-            'cluster_id': this.city.cluster_id,
-            'mini_grid_id': this.city.mini_grid_id,
-
-        };
-        try {
-            let response = await this.repository.create(city_PM);
-            if (response.status === 200 || response.status === 201) {
-                this.city.id = response.data.data.id;
-                EventBus.$emit('Village Added', this.city)
+                this.cities = response.data.data
+                return this.cities
             } else {
-                return new ErrorHandler(response.error, 'http', response.status);
+                return new ErrorHandler(response.error, 'http', response.status)
+            }
+        } catch (e) {
+            return new ErrorHandler(e, 'http')
+        }
+    }
+
+    async createCity (name, cluster_id, minigrid_id, geo_data) {
+        try {
+            let city_PM = {
+                'name': name,
+                'cluster_id': cluster_id,
+                'mini_grid_id': minigrid_id,
+                'geo_data': geo_data
+            }
+
+            let response = await this.repository.create(city_PM)
+            if (response.status === 200 || response.status === 201) {
+                this.city = response.data.data
+                return response
+            } else {
+                return new ErrorHandler(response.error, 'http', response.status)
             }
 
         } catch (e) {
-            return new ErrorHandler(e, 'http');
+            let erorMessage = e.response.data.data.message
+            return new ErrorHandler(erorMessage, 'http')
         }
 
     }
