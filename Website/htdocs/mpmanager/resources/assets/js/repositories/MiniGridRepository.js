9fdc344dce67f75c9241941bc29ed3e6ec72d5c8
93Kamuran <enginkamuran@gmail.com>
2020-06-15 14:07:49 +0300

    MiniGrid repository added.

--- Website/htdocs/mpmanager/resources/assets/js/repositories/MiniGridRepository.js
+++ Website/htdocs/mpmanager/resources/assets/js/repositories/MiniGridRepository.js
@@ -1,8 +1,17 @@
-const resource = '/api/mini-grids';
-
+const resource = '/api/mini-grids'
 
 export default {
-    list() {
-        return axios.get(`${resource}`);
+    list () {
+        return axios.get(`${resource}`)
+    },
+    create (miniGrid_PM) {
+
+        return axios.post(`${resource}`, miniGrid_PM)
+    },
+    get (miniGrid_Id) {
+        return axios.get(`${resource}/${miniGrid_Id}`)
+    },
+    geoData (miniGrid_Id) {
+        return axios.get(`${resource}/${miniGrid_Id}?relation=1`)
     }
 }
