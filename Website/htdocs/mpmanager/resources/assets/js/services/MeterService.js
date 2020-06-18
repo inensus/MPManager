0c8ff8a1d754c422b4c984b22b0d3f4fcb471a7f
93Kamuran <enginkamuran@gmail.com>
2020-06-18 12:45:51 +0300

    Person & Meter services added.

---
+++ Website/htdocs/mpmanager/resources/assets/js/services/MeterService.js
@@ -0,0 +1,98 @@
+import RepositoryFactory from '../repositories/RepositoryFactory'
+import { ErrorHandler } from '../Helpers/ErrorHander'
+
+export class MeterService {
+    constructor () {
+        this.repository = RepositoryFactory.get('meter')
+        this.meters = []
+        this.selectedMeter = null
+        this.meter = {
+            id: null,
+            meter_parameter: null,
+            serial_number: null,
+            max_current: null,
+            phase: null,
+            tariff: {
+                id: null,
+                name: null,
+                price: null
+            },
+            geo: []
+
+        }
+    }
+
+    async getMeterGeos (miniGridId) {
+        try {
+            let response = await this.repository.geoList(miniGridId)
+
+            if (response.status === 200) {
+                this.meters = response.data.data
+                return this.meters
+            } else {
+                return new ErrorHandler(response.error, 'http', response.status)
+            }
+        } catch (e) {
+            return new ErrorHandler(e, 'http')
+        }
+    }
+
+    async getMeterDetails (meterId) {
+        try {
+            let response = await this.repository.get(meterId)
+
+            if (response.status === 200) {
+                let data = response.data.data
+                let points = [0, 0]
+                if (data.meter_parameter.geo != null) {
+                    points = data.meter_parameter.geo.points.split(',')
+                }
+                this.meter = {
+                    id: meterId,
+                    'meter_parameter': data.meter_parameter,
+                    'serial_number': data.serial_number,
+                    'max_current': data.meter_type.max_current,
+                    'phase': data.meter_type.phase,
+                    'tariff': {
+                        'id': data.meter_parameter.tariff.id,
+                        'name': data.meter_parameter.tariff.name,
+                        'price': data.meter_parameter.tariff.price
+                    },
+                    'geo': [points[0], points[1]]
+
+                }
+                return this.meter
+            } else {
+                return new ErrorHandler(response.error, 'http', response.status)
+            }
+        } catch (e) {
+            return new ErrorHandler(e, 'http')
+        }
+    }
+
+    async updateMeter (meterId, lat, lng) {
+        try {
+            let points_PM = {
+                lat: lat,
+                lng: lng
+            }
+            let response = await this.repository.update(meterId,points_PM)
+            if (response.status === 200) {
+
+                return response
+            } else {
+                return new ErrorHandler(response.error, 'http', response.status)
+            }
+        } catch (e) {
+            return new ErrorHandler(e, 'http')
+        }
+    }
+
+    getMeters () {
+        return this.meters
+    }
+
+    addMeter (meter) {
+        this.meters.push(meter)
+    }
+}
---
+++ Website/htdocs/mpmanager/resources/assets/js/services/PersonService.js
@@ -0,0 +1,98 @@
+import Repository from '../repositories/RepositoryFactory'
+import { ErrorHandler } from '../Helpers/ErrorHander'
+
+
+export class PersonService {
+    constructor () {
+        this.repository = new Repository.get('person')
+        this.person = {
+            id: null,
+            title: null,
+            education: null,
+            birthDate: null,
+            name: null,
+            surname: null,
+            gender: null,
+            nationality: null,
+            city: null,
+            meters: []
+        }
+        this.fullName = null
+    }
+
+    async getPerson (personId) {
+        try {
+
+            let response = await this.repository.get(personId)
+
+            if (response.status === 200 || response.status === 201) {
+                let personData = response.data.data
+                this.person = {
+                    id: personData.id,
+                    title: personData.title,
+                    education: personData.education,
+                    birthDate: personData.birth_date,
+                    name: personData.name,
+                    surname: personData.surname,
+                    nationality: personData.citizenship != null ? personData.citizenship.country_name : 'No data available',
+                    gender: personData.sex,
+                    addresses: personData.addresses,
+                    meters: personData.meters
+                }
+                return this.person
+            } else {
+                return new ErrorHandler(response.error, 'http', response.status)
+            }
+        } catch (e) {
+            let errorMessage = e.response.data.data.message
+            return new ErrorHandler(errorMessage, 'http')
+
+        }
+
+    }
+
+    getFullName () {
+        this.fullName = this.person.name + ' ' + this.person.surname
+        return this.fullName
+    }
+
+    getId () {
+        return this.person.id
+    }
+
+    async updatePerson () {
+        try {
+            this.updateName(this.person.name)
+            if (this.person.birthDate !== null) {
+                this.person.birthDate = this.isoYear(this.person.birthDate)
+            }
+            let response = await this.repository.update(this.person)
+            if (response.status === 200 || response.status === 201) {
+                return response.data.data
+            } else {
+                return new ErrorHandler(response.error, 'http', response.status)
+            }
+        } catch (e) {
+            let errorMessage = e.response.data.data.message
+            return new ErrorHandler(errorMessage, 'http')
+
+        }
+    }
+
+    isoYear (date) {
+        return moment(date).format('YYYY-MM-DD')
+
+    }
+
+    updateName (fullName) {
+        let x = fullName.split(' ')
+        if (x.length < 2) {
+            return {
+                'success': false,
+
+            }
+        }
+        this.person.surname = x.splice(-1)
+        this.person.name = x.join(' ')
+    }
+}
