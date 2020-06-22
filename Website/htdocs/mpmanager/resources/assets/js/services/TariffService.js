1a173b1cda8b7d9955bea46aba37a71488b8280b
93Kamuran <enginkamuran@gmail.com>
2020-06-22 07:26:07 +0300

    TariffService.js refactored with social tariff and component base tariff options.

--- Website/htdocs/mpmanager/resources/assets/js/services/TariffService.js
+++ Website/htdocs/mpmanager/resources/assets/js/services/TariffService.js
@@ -1,34 +1,42 @@
 import Repository from '../repositories/RepositoryFactory'
-import {AccessRate} from "../classes/AccessRate";
-import {EventBus} from "../shared/eventbus";
-import {ErrorHandler} from "../Helpers/ErrorHander";
+import { AccessRate } from '../classes/AccessRate'
+import { EventBus } from '../shared/eventbus'
+import { ErrorHandler } from '../Helpers/ErrorHander'
 
 export class TariffService {
-    constructor() {
-        this.repository = Repository.get('tariff');
-        this.list = [];
+    constructor () {
+        this.repository = Repository.get('tariff')
+        this.list = []
         this.tariff = {
             id: 0,
             name: '',
-            price: 0,
+            price: null,
             currency: 'TZS',
             factor: 1,
             accessRate: null
 
         }
+        this.socialTariff =
+            {
+                dailyAllowance: null,
+                price: 0,
+                initialEnergyBudget: 0,
+                maximumStackedEnergy: 0
+            }
+        this.components = []
 
     }
 
-    async getTariffs() {
+    async getTariffs ()  {
         try {
-            let response = await this.repository.list();
+            let response = await this.repository.list()
             if (response.status === 200 || response.status === 201) {
-                let data = response.data.data;
+                let data = response.data.data
                 for (let i in data) {
 
-                    let tariff_data = data[i];
-                    let tariff_data_access_rate = tariff_data.access_rate;
-                    let accessRate = null;
+                    let tariff_data = data[i]
+                    let tariff_data_access_rate = tariff_data.access_rate
+                    let accessRate = null
                     if (tariff_data_access_rate !== undefined) {
                         accessRate = new AccessRate(
                             tariff_data_access_rate.id,
@@ -43,20 +51,21 @@ export class TariffService {
                         currency: tariff_data.currency,
                         factor: tariff_data.factor,
                         accessRate: accessRate,
-                    };
+                    }
                     this.list.push(tariff)
                 }
-                return this.list;
+                return this.list
             } else {
-                return new ErrorHandler(response.error, 'http', response.status);
+                return new ErrorHandler(response.error, 'http', response.status)
             }
         } catch (e) {
-            return new ErrorHandler(e, 'http');
+            let errorMessage = e.response.data.data.message
+            return new ErrorHandler(errorMessage, 'http')
         }
 
     }
 
-    async createTariff() {
+    async createTariff () {
         let tariff_PM = {
             'name': this.tariff.name,
             'price': this.tariff.price,
@@ -64,25 +73,68 @@ export class TariffService {
             'factor': this.tariff.factor,
             'access_rate_period': this.tariff.accessRate.period,
             'access_rate_amount': this.tariff.accessRate.amount,
-
-        };
+            'components': this.components,
+            'social_tariff': {
+                'daily_allowance': this.socialTariff.dailyAllowance,
+                'price': this.socialTariff.price,
+                'initial_energy_budget': this.socialTariff.initialEnergyBudget,
+                'maximum_stacked_energy': this.socialTariff.maximumStackedEnergy
+            }
+        }
         try {
-            let response = await this.repository.create(tariff_PM);
+            let response = await this.repository.create(tariff_PM)
             if (response.status === 200 || response.status === 201) {
-                this.tariff.id = response.data.data.id;
+                this.tariff.id = response.data.data.id
                 EventBus.$emit('tariffAdded', this.tariff)
             } else {
-                return new ErrorHandler(response.error, 'http', response.status);
+                return new ErrorHandler(response.error, 'http', response.status)
             }
 
         } catch (e) {
-            return new ErrorHandler(e, 'http');
+
+            let errorMessage = e.response.data.data.message
+            return new ErrorHandler(errorMessage, 'http')
         }
 
     }
 
-    setAccessRate(accessRate) {
+    setAccessRate (accessRate) {
         this.tariff.accessRate = accessRate
     }
 
+    addAdditionalCostComponent () {
+        let lastComponent = this.components[this.components.length - 1]
+        if (lastComponent !== null && lastComponent !== undefined) {
+            let component = {
+                id: lastComponent.id + 1,
+                name: '',
+                price: 0
+            }
+            this.components.push(component)
+        } else {
+            let component = {
+                id: 1,
+                name: '',
+                price: null
+            }
+            this.components.push(component)
+        }
+
+        return this.components
+    }
+
+    removeAdditionalComponent (id) {
+        let component = this.components.filter(x => x.id === id)[0]
+        if (component !== null) {
+            for (let i = 0; i < this.components.length; i++) {
+                if (this.components[i].id === component.id) {
+                    this.components.splice(i, 1)
+                }
+            }
+            return this.components
+        } else {
+            return this.components
+        }
+    }
 }
+
