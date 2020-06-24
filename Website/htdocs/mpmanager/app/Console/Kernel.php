fb18ab7a29d87577f43c36fe125f1b54ff8b5e99
Kemal <alchalade@gmail.com>
2020-06-24 11:29:24 +0300

    Schedule SocialTariffPiggyBankManager to midnight

--- Website/htdocs/mpmanager/app/Console/Kernel.php
+++ Website/htdocs/mpmanager/app/Console/Kernel.php
@@ -2,46 +2,48 @@
 
 namespace App\Console;
 
+use App\Jobs\SocialTariffPiggyBankManager;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 
 class Kernel extends ConsoleKernel
 {
-	/**
-	 * The Artisan commands provided by your application.
-	 *
-	 * @var array
-	 */
-	protected $commands = [
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
 
-		Commands\AddMeterAddress::class,
-	];
+        Commands\AddMeterAddress::class,
+    ];
 
-	/**
-	 * Define the application's command schedule.
-	 *
-	 * @param Schedule $schedule
-	 *
-	 * @return void
-	 */
-	protected function schedule(Schedule $schedule)
-	{
-		// $schedule->command('inspire')
-		//          ->hourly();
-		$schedule->command('accessrate:check')->dailyAt('18:00');
-		$schedule->command('calinMeters:readOnline')->dailyAt('04:00');
-	}
+    /**
+     * Define the application's command schedule.
+     *
+     * @param Schedule $schedule
+     *
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')
+        //          ->hourly();
+        $schedule->command('accessrate:check')->daily();
+        $schedule->command('calinMeters:readOnline')->dailyAt('04:00');
+        $schedule->job(new SocialTariffPiggyBankManager)->daily();
+    }
 
-	/**
-	 * Register the commands for the application.
-	 *
-	 * @return void
-	 */
-	protected function commands()
-	{
-		$this->load(__DIR__ . '/Commands');
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
 
-		require base_path('routes/console.php');
-	}
+        require base_path('routes/console.php');
+    }
 }
