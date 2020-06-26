d381ae074beb8a6a82b8a70df1e5c8e8a692c526
Kemal <alchalade@gmail.com>
2020-06-26 10:08:47 +0300

    social tariff

---
+++ Website/htdocs/mpmanager/app/Models/SocialTariff.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SocialTariff
+ * @package App\Models
+ *
+ * @property int $id
+ * @property int $tariff_id
+ * @property int $daily_allowance
+ * @property int $price
+ * @property int $initial_energy_budget
+ * @property int $maximum_stacked_energy
+ */
+class SocialTariff extends Model
+{
+    protected $guarded = [];
+
+}
