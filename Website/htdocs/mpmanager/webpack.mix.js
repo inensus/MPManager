ab548bfd364da747963359d71f5a14c47b383338
Kemal <alchalade@gmail.com>
2020-07-08 18:48:52 +0300

    add gzip compression

--- Website/htdocs/mpmanager/webpack.mix.js
+++ Website/htdocs/mpmanager/webpack.mix.js
@@ -1,5 +1,19 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix')
 
+mix.js('resources/assets/js/app.js', 'public/js').
+    sass('resources/assets/sass/app.scss', 'public/css')
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+const CompressionPlugin = require('compression-webpack-plugin')
+
+mix.webpackConfig({
+    plugins: [
+
+        new CompressionPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.js$|\.css$|\.html$|\.svg$/,
+            threshold: 10240,
+            minRatio: 0.8,
+        }),
+    ],
+})
