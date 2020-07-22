84b349219000ea736a915d329026a40c9520a316
93Kamuran <enginkamuran@gmail.com>
2020-07-22 11:08:44 +0300

    migrations of Agents table added.

---
+++ Website/htdocs/mpmanager/database/migrations/2020_07_13_132006_create_agent_balance_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgentBalanceHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agent_balance_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('agent_id');
+            $table->unsignedInteger('user_id');
+            $table->decimal('amount', 12, 4);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agent_balance_histories');
+    }
+}
