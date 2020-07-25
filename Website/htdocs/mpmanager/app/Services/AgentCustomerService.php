19b04ad1e92091459ced10effedbb148a6c5ede7
93Kamuran <enginkamuran@gmail.com>
2020-07-25 13:05:35 +0300

    AgentCustomersSearch method added.

--- Website/htdocs/mpmanager/app/Services/AgentCustomerService.php
+++ Website/htdocs/mpmanager/app/Services/AgentCustomerService.php
@@ -20,8 +20,8 @@ class AgentCustomerService
     public function list(Agent $agent)
     {
 
-     $miniGridId = $agent->mini_grid_id;
-      return Person::with([
+        $miniGridId = $agent->mini_grid_id;
+        return Person::with([
             'addresses' => function ($q) {
                 return $q->where('is_primary', 1);
             },
@@ -29,7 +29,7 @@ class AgentCustomerService
             'meters.meter',
         ])
             ->where('is_customer', 1)
-            ->whereHas('addresses',function ($q) use ($miniGridId){
+            ->whereHas('addresses', function ($q) use ($miniGridId) {
                 $q->whereHas('city', function ($q) use ($miniGridId) {
                     $q->where('mini_grid_id', $miniGridId);
                 });
@@ -37,9 +37,42 @@ class AgentCustomerService
             ->paginate(config('settings.paginate'));
 
 
-
-
     }
 
+    public function searchAgentsCustomers($searchTerm, $paginate, $agent)
+    {
+
+        $miniGridId = $agent->mini_grid_id;
+        $personQuery =Person::with([
+            'addresses' => function ($q) {
+                return $q->where('is_primary', 1);
+            },
+            'addresses.city',
+            'meters.meter',
+
+        ])->where('is_customer', 1)
+            ->where('name', 'LIKE', '%' . $searchTerm . '%')
+            ->whereHas('addresses.city', function ($q) use ($searchTerm, $miniGridId) {
+                $q->where('mini_grid_id', $miniGridId);
+            })
+            ->orWhere('surname', 'LIKE', '%' . $searchTerm . '%')
+            ->orWhereHas('addresses', function ($q) use ($searchTerm) {
+                $q->where('email', 'LIKE', '%' . $searchTerm . '%');
+                $q->where('phone', 'LIKE', '%' . $searchTerm . '%');
+            })
+            ->orWhereHas('addresses.city', function ($q) use ($searchTerm) {
+                $q->where('name', 'LIKE', '%' . $searchTerm . '%');
+            })
+            ->orWhereHas('meters.meter', function ($q) use ($searchTerm) {
+                $q->where('serial_number', 'LIKE', '%' . $searchTerm . '%');
+            });
+
+        if ($paginate === 1) {
+            return $personQuery->paginate(15);
+        }
+
+        return $personQuery->get();
+
+    }
 
 }
