5435f5ffeda26a73b33f2685afb6c766b58c005b
93Kamuran <enginkamuran@gmail.com>
2020-07-25 12:54:00 +0300

    - AgentCommissionController.php & CreateAgentCommissionRequest.php added. - AgentBalanceMiddleware.php added.

---
+++ Website/htdocs/mpmanager/app/Http/Controllers/AgentCommissionController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateAgentCommissionRequest;
+use App\Http\Resources\ApiResource;
+use App\Models\AgentCommission;
+use Illuminate\Http\Request;
+
+class AgentCommissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param CreateAgentCommissionRequest $request
+     * @return ApiResource
+     */
+    public function store(CreateAgentCommissionRequest $request)
+    {
+        $commission = AgentCommission::query()->create(request()->only('name',
+            'energy_commission',
+            'appliance_commission',
+            'risk_balance'));
+        return new ApiResource($commission);
+    }
+
+    /**
+     * Display the specified resourc
+     *
+     * @param AgentCommission $commission
+     * @return void
+     */
+    public function show(AgentCommission $commission)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Commission $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commission $commission)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Commission $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commission $commission)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Commission $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commission $commission)
+    {
+        //
+    }
+}
---
+++ Website/htdocs/mpmanager/app/Http/Middleware/AgentBalanceMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\AgentRiskBalanceExceeded;
+use App\Exceptions\DownPaymentNotFoundException;
+use App\Exceptions\TransactionAmountNotFoundException;
+use App\Models\AgentAssignedAppliances;
+use Closure;
+
+class AgentBalanceMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        echo "hayvan5";
+        die;
+        $routeName = request()->route()->getName();
+        $agent = auth('agent_api')->user();
+        $commission = $agent->commission()->first();
+        $amount = $agent->balance;
+        if ($routeName === 'agent-sell-appliance') {
+            if ($downPayment = $request->input('down_payment')) {
+                $amount -= $downPayment;
+            } else {
+                throw  new DownPaymentNotFoundException('DownPayment not found');
+            }
+
+        }
+        if ($routeName === 'agent-transaction') {
+            if ($transactionAmount = $request->input('amount')) {
+                $amount -= $transactionAmount;
+            } else {
+                throw  new TransactionAmountNotFoundException('Transaction amount not found');
+            }
+
+
+        }
+
+        if ($amount < $commission->risk_balance) {
+            throw  new AgentRiskBalanceExceeded('Risk balance exceeded');
+        }
+        return $next($request);
+    }
+}
