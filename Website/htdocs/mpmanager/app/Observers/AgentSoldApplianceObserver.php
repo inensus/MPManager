efd2046f8a4ed07eceb2c3a9dbdccc03583f552d
Kemal <alchalade@gmail.com>
2020-07-25 16:13:06 +0300

    create agent transaction and balance histories when after an agent sold appliance

--- Website/htdocs/mpmanager/app/Observers/AgentSoldApplianceObserver.php
+++ Website/htdocs/mpmanager/app/Observers/AgentSoldApplianceObserver.php
@@ -4,9 +4,14 @@
 namespace App\Observers;
 
 
+use App\Models\Agent;
 use App\Models\AgentAssignedAppliances;
 use App\Models\AgentBalanceHistory;
+use App\Models\AgentCommission;
 use App\Models\AgentSoldAppliance;
+use App\Models\Person\Person;
+use App\Models\Transaction\AgentTransaction;
+use App\Models\Transaction\Transaction;
 
 class AgentSoldApplianceObserver
 {
@@ -20,13 +25,59 @@ class AgentSoldApplianceObserver
 
     public function created(AgentSoldAppliance $appliances)
     {
-         $assignedApplianceId= $appliances->agent_assigned_appliance_id;
-         $assignedAppliance = AgentAssignedAppliances::find($assignedApplianceId);
 
-        AgentBalanceHistory::query()->create([
-            'agent_id' => $assignedAppliance->agent_id,
-            'amount' => (-1 * $assignedAppliance->cost),
-            'type' => 'Appliance'
+        $assignedApplianceId = $appliances->agent_assigned_appliance_id;
+        $assignedAppliance = AgentAssignedAppliances::query()->find($assignedApplianceId);
+
+        $appliance = $assignedAppliance->applianceType()->first();
+        $agent = Agent::query()->find($assignedAppliance->agent_id);
+        $buyer = Person::query()->find(request()->input('person_id'));
+        //create agent transaction
+        $agentTransaction = AgentTransaction::query()->create([
+            'agent_id' => $agent->id,
+            'device_id' => $agent->device_id,
+            'status' => 1,
         ]);
+
+        $transaction = Transaction::query()->make([
+            'amount' => request()->input('down_payment'),
+            'sender' => $agent->device_id,
+            'message' => '-',
+        ]);
+        $transaction->originalTransaction()->associate($agentTransaction);
+        $transaction->save();
+
+        event('payment.successful', [
+            'amount' => $transaction->amount,
+            'paymentService' => 'agent',
+            'paymentType' => 'appliance',
+            'sender' => $agent->device_id,
+            'paidFor' => $appliance,
+            'payer' => $buyer,
+            'transaction' => $transaction,
+        ]);
+
+        $history = AgentBalanceHistory::query()->make([
+            'agent_id' => $agent->id,
+            'amount' => (-1 * request()->input('down_payment')),
+            'transaction_id' => $transaction->id,
+
+        ]);
+        $history->trigger()->associate($assignedAppliance);
+        $history->save();
+
+
+        //create agent commission
+        $commission = AgentCommission::query()->find($agent->agent_commission_id);
+
+        $history = AgentBalanceHistory::query()->make([
+            'agent_id' => $agent->id,
+            'amount' => ($assignedAppliance->cost * $commission->appliance_commission),
+            'transaction_id' => $transaction->id,
+        ]);
+        $history->trigger()->associate($commission);
+        $history->save();
+
+
     }
 }
