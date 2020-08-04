2c4d619559d402751055415eadb6aac8affcf237
93Kamuran <enginkamuran@gmail.com>
2020-08-04 17:42:15 +0300

    AgentCharge relation added to AgentBalanceHistory.

--- Website/htdocs/mpmanager/app/Models/AgentBalanceHistory.php
+++ Website/htdocs/mpmanager/app/Models/AgentBalanceHistory.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Transaction\Transaction;
+use App\Relations\BelongsToMorph;
 use Illuminate\Database\Eloquent\Model;
 
 class AgentBalanceHistory extends Model
@@ -19,7 +20,10 @@ class AgentBalanceHistory extends Model
     {
         return $this->morphTo();
     }
-
+    public function triggerCharge()
+    {
+        return BelongsToMorph::build($this, AgentCharge::class, 'trigger');
+    }
     public function transaction()
     {
         return $this->belongsTo(Transaction::class);
