242f7f01e253f2bf20c894cb70bcda67f922a221
93Kamuran <enginkamuran@gmail.com>
2020-08-05 22:33:39 +0300

    AgentBalanceHistoryObserver.php refactored.

--- Website/htdocs/mpmanager/app/Observers/AgentBalanceHistoryObserver.php
+++ Website/htdocs/mpmanager/app/Observers/AgentBalanceHistoryObserver.php
@@ -7,7 +7,10 @@ namespace App\Observers;
 use App\Models\Agent;
 use App\Models\AgentAssignedAppliances;
 use App\Models\AgentBalanceHistory;
+use App\Models\AgentCharge;
 use App\Models\AgentCommission;
+use App\Models\AgentReceipt;
+use App\Models\AgentSoldAppliance;
 
 class AgentBalanceHistoryObserver
 {
@@ -28,8 +31,34 @@ class AgentBalanceHistoryObserver
     {
         $trigger = $agentBalanceHistory->trigger()->first();
         $agent = Agent::query()->find($agentBalanceHistory->agent_id);
-        $agent->balance += $agentBalanceHistory->amount;
-        $agent->update();
-        //
+        if ($trigger instanceof AgentAssignedAppliances) {
+            if ($agent->balance < 0) {
+                $agent->due_to_energy_supplier += (-1 * $agentBalanceHistory->amount);
+                $agent->balance += $agentBalanceHistory->amount;
+            } else {
+                if ($agent->balance < (-1 * $agentBalanceHistory->amount)) {
+                    $agent->due_to_energy_supplier += -1 * ($agent->balance + $agentBalanceHistory->amount);
+                    $agent->balance += $agentBalanceHistory->amount;
+                } else {
+                    $agent->balance += $agentBalanceHistory->amount;
+                }
+            }
+            $agent->update();
+        } elseif ($trigger instanceof AgentCommission) {
+            $agent->commission_revenue += $agentBalanceHistory->amount;
+            if ($agent->balance < 0) {
+                $agent->due_to_energy_supplier += (-1 * $agentBalanceHistory->amount);
+            }
+            $agent->update();
+        } elseif ($trigger instanceof AgentCharge) {
+            $agent->balance += $agentBalanceHistory->amount;
+            $agent->update();
+        } elseif ($trigger instanceof AgentReceipt) {
+            $agent->due_to_energy_supplier -= $agentBalanceHistory->amount;
+            $agent->balance += $agentBalanceHistory->amount + $agent->commission_revenue;
+            $agent->commission_revenue = 0;
+            $agent->update();
+        }
+
     }
 }
