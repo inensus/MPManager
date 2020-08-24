e63a118b4126c0dde50607c5b02dc8077bd11c11
kamuran engin <43153928+93Kamuran@users.noreply.github.com>
2020-08-24 12:18:48 +0300

    Assigning new asset to customer (#70)
    * AssetPersonRepository.js & AssetPersonService.js added.
    
    * AssetRateRepository.js.js & AssetRateService.js.js added.
    
    * AssetPersonRepository & AssetRateRepository defined to RepositoryFactory.js.
    
    * AssetRepository.js.js & AssetService.js.js refactored.
    
    * DeferredPayments.vue refactored.
    
    * spinner icon removed.
    
    Co-authored-by: Kemal <Alchalade@gmail.com>

---
+++ Website/htdocs/mpmanager/resources/assets/js/services/AssetPersonService.js
@@ -0,0 +1,44 @@
+import Repository from '../repositories/RepositoryFactory'
+import {Paginator} from "../classes/paginator"
+import {ErrorHandler} from "../Helpers/ErrorHander"
+
+export class AssetPersonService {
+    constructor() {
+        this.repository = Repository.get('assetPerson')
+        this.list = []
+
+
+    }
+    async getPersonAssets(id){
+        try {
+            let response = await this.repository.list(id)
+            if (response.status === 200 || response.status === 201) {
+                this.list=response.data.data
+                return this.list
+            } else {
+                new ErrorHandler(response.error, 'http', response.status)
+            }
+
+        } catch (e) {
+            let errorMessage = e.response.data.data.message
+            return new ErrorHandler(errorMessage, 'http')
+        }
+    }
+
+    async saveAsset(id,personId,assetPM){
+        try {
+            let response = await this.repository.create(id,personId,assetPM)
+            if (response.status === 200 || response.status === 201) {
+
+                return response
+            } else {
+                new ErrorHandler(response.error, 'http', response.status)
+            }
+
+        } catch (e) {
+            let errorMessage = e.response.data.data.message
+            return new ErrorHandler(errorMessage, 'http')
+        }
+    }
+
+}
