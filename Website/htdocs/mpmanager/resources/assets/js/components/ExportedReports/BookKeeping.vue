d3d66107cd93c3836e55825e5efd80714ef67f96
93Kamuran <enginkamuran@gmail.com>
2020-09-18 12:59:26 +0300

    BookKeeping.vue & Reports.vue added. ReportsList.vue refactored with new added components.

---
+++ Website/htdocs/mpmanager/resources/assets/js/components/ExportedReports/BookKeeping.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <widget
+            :id="'book-keeping'"
+            :title="'Payment Requests'"
+            :paginator="bookKeepingService.paginator"
+            :search="false"
+            :subscriber="subscriber"
+            color="orange"
+        >
+            <div v-if="list.length>0">
+          <md-table v-model="list" md-sort="id" md-sort-order="desc">
+              <md-table-row>
+                  <md-table-head v-for="(item, index) in headers" :key="index">{{item}}</md-table-head>
+              </md-table-row>
+                <md-table-row  slot="md-table-row" slot-scope="{ item }">
+                    <md-table-cell md-sort-by="id" md-label="ID"> {{ item.id}}</md-table-cell>
+                    <md-table-cell md-label="Date"> {{ item.date}}</md-table-cell>
+                    <md-table-cell md-label="File">
+                        <div @click="download(item.id,'/book-keeping')">
+                            <font-awesome-icon icon="save" style="cursor: pointer;"/>
+                            <span > Download</span>
+                        </div>
+
+                    </md-table-cell>
+                </md-table-row>
+            </md-table>
+            </div>
+            <div v-else>
+                <no-table-data :headers="headers" :tableName="tableName"/>
+            </div>
+        </widget>
+    </div>
+
+</template>
+
+<script>
+import Widget from '../../shared/widget'
+import {EventBus} from '../../shared/eventbus'
+import { BookKeepingService } from '../../services/BookKeepingService'
+import NoTableData from '../../shared/NoTableData'
+
+export default {
+    name: 'BookKeeping',
+    components: {
+        Widget,NoTableData
+    },
+    mounted() {
+
+        EventBus.$on('pageLoaded', this.reloadList)
+
+    },
+    beforeDestroy() {
+        EventBus.$off('pageLoaded', this.reloadList)
+    },
+    data() {
+        return {
+            bookKeepingService: new BookKeepingService(),
+            list: [],
+            subscriber: 'bookKeeping',
+            headers: ['ID', 'Date', 'File'],
+            tableName: 'Outsource Report'
+        }
+    },
+    methods: {
+        reloadList(subscriber, data) {
+
+            if (subscriber === this.subscriber) {
+                this.list = this.bookKeepingService.updateList(data)
+            }
+        },
+        endSearching() {
+            this.bookKeeping.showAll()
+        },
+        download(id, reference) {
+            window.open(this.bookKeepingService.exportBookKeeping(id,reference))
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
