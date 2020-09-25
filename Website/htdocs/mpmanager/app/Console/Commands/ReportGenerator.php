1a1e3bbbc8d19ecc7ece43d91d17ea3ec99afb46
kamuran engin <43153928+93Kamuran@users.noreply.github.com>
2020-09-25 15:50:53 +0300

    Generating Reports (#106)
    * BookKeeping.vue & Reports.vue added. ReportsList.vue refactored with new added components.
    
    * Repository pattern applied to city revenue reports and PaymentHistory reports.
    
    * Assigning person bug fixed in Ticket.vue.
    
    * Target table name changed on  OutsourceReportModel.
    
    * outsourceWithJob function added for creating outsource reports with laravel console command
    
    * Report.php model refactored with extending BaseModel.
    
    * generateWithJob function added for creating city revenue reports with laravel console command
    
    * OutsourceReportGenerator.php & ReportGenerator.php console commands added for creating reports on laravel console.
    
    * OutsourceReportGenerator  ReportGenerator  commands added to schedule for generating weekly & monthly reports.
    
    * requested changes resolved.
    
    * requested changes resolved.
    
    * requested changes resolved.
    
    * File names have transported to URL friendly strings.

---
+++ Website/htdocs/mpmanager/app/Console/Commands/ReportGenerator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\Export\Reports;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class ReportGenerator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reports:city-revenue {type} {--start-date=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates city revenue reports';
+
+    public $reports;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param Reports $reports
+     */
+    public function __construct(Reports $reports)
+    {
+        parent::__construct();
+        $this->reports = $reports;
+
+    }
+
+
+    /**
+     * Execute the console command.
+     */
+    public function handle():void
+    {
+
+        $startDay = Carbon::now()->format('Y-m-d');
+        if ($this->option('start-date') != "") {
+            $toDay = Carbon::parse($this->option('start-date'))->format('Y-m-d');
+        } else {
+
+            $toDay = Carbon::now()->subDays(1)->format('Y-m-d');
+        }
+        if ($this->argument('type') == "weekly") {
+            $startDay = Carbon::parse($toDay)->modify("last Monday")->format('Y-m-d');
+        } elseif ($this->argument('type') == "monthly") {
+            $startDay = Carbon::parse($toDay)->modify("first day of this month")->format('Y-m-d');
+        }else{
+            var_dump("That the given parameter is not supported and end the process with that");
+        }
+        $this->reports->generateWithJob($startDay, $toDay, $this->argument('type'));
+    }
+}
--- Website/htdocs/mpmanager/packages/inensus/ticket/src/app/Http/Controllers/ExportController.php
+++ Website/htdocs/mpmanager/packages/inensus/ticket/src/app/Http/Controllers/ExportController.php
@@ -5,6 +5,7 @@ namespace Inensus\Ticket\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Inensus\Ticket\Http\Resources\TicketResource;
 use Inensus\Ticket\Models\Label;
 use Inensus\Ticket\Models\OutsourceReport;
@@ -75,11 +76,13 @@ class ExportController
             ->whereHas('category', static function ($q) {
                 $q->where('out_source', 1);
             })
+            ->whereHas('assignedTo', static function ($q) {
+                $q->whereNotNull('owner_id');
+            })
             ->whereBetween('created_at', [$startDate, $endDate])
             ->get();
         //create excel sheet
-
-        $fileName = 'Outsourcing - ' . $startDate . ' - ' . $endDate . ' .xlsx';
+        $fileName = 'Outsourcing-' . $startDate . '-' . $endDate . '.xlsx';
 
         $sheet = $this->spreadsheet->getActiveSheet();
         $sheet->setTitle('payments - ' . date('Y-m', strtotime($startDate)));
@@ -96,8 +99,11 @@ class ExportController
             $sheet->setCellValue('C' . $row, $t->outsource->amount);
             $sheet->setCellValue('D' . $row, $t->category->label_name);
         }
-
         $writer = new Xlsx($this->spreadsheet);
+        $dirPath = storage_path('./outsourcing');
+        if (!file_exists($dirPath)) {
+            mkdir($dirPath, 0774, true);
+        }
         try {
             $writer->save(storage_path('./outsourcing/' . $fileName));
         } catch (Exception $e) {
@@ -110,6 +116,57 @@ class ExportController
         return new TicketResource($this->outsourceReport);
     }
 
+    public function outsourceWithJob($startDate,$endDate): void
+    {
+        try {
+            $tickets = $this->ticket
+                ::with('outsource', 'owner', 'category')
+                ->whereHas('category', static function ($q) {
+                    $q->where('out_source', 1);
+                })
+                ->whereBetween('created_at', [$startDate, $endDate])
+                ->get();
+            //create excel sheet
+            $fileName = 'Outsourcing-' . $startDate . '-' . $endDate . '.xlsx';
+
+            $sheet = $this->spreadsheet->getActiveSheet();
+            $sheet->setTitle('payments - ' . date('Y-m', strtotime($startDate)));
+
+            $sheet->setCellValue('A1', 'Name');
+            $sheet->setCellValue('B1', 'Date');
+            $sheet->setCellValue('C1', 'Amount');
+            $sheet->setCellValue('D1', 'Category');
+
+            $row = 3;
+            foreach ($tickets as $t) {
+                $owner = $t->owner !== null ? $t->owner->name .' '.$t->owner->surname:"No assigned user found, please check your history reports";
+                $sheet->setCellValue('A' . $row, $owner);
+                $sheet->setCellValue('B' . $row, $t->created_at);
+                $sheet->setCellValue('C' . $row, $t->outsource->amount);
+                $sheet->setCellValue('D' . $row, $t->category->label_name);
+            }
+            $writer = new Xlsx($this->spreadsheet);
+            $dirPath = storage_path('./outsourcing');
+            if (!file_exists($dirPath)) {
+                mkdir($dirPath, 0774, true);
+            }
+            try {
+                $writer->save(storage_path('./outsourcing/' . $fileName));
+            } catch (Exception $e) {
+                echo 'error' . $e->getMessage();
+            }
+
+            $this->outsourceReport->date = $startDate . '---' . $endDate;
+            $this->outsourceReport->path = storage_path('./outsourcing/' . $fileName);
+            $this->outsourceReport->save();
+
+        }catch (\Exception $e){
+            Log::critical('Outsource report job failed.',
+                ['Exception' => $e->getMessage()]
+            );
+        }
+
+    }
     public function download($id): BinaryFileResponse
     {
         $report = $this->outsourceReport->find($id);
