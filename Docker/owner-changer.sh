b45d225986195d35d8f4de5efe262d25a4865957
93Kamuran <enginkamuran@gmail.com>
2020-10-26 16:36:27 +0300

    parametrically echos are applied

--- Docker/owner-changer.sh
+++ Docker/owner-changer.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Collect static files
-
 if id "$1" >/dev/null 2>&1; then
-        echo "www-data user exist."
+        echo "${1} user exist."
         echo "changing volume owner.."
         chown -R www-data:www-data /var/www/html/mpmanager
         echo "Volume owner changed to www-data."
 else
-         echo "www-data user does not exist."
+         echo "${1} user does not exist."
          echo "Volume owner could not be changed, please adjust your permission setting for web server."
 fi
 
