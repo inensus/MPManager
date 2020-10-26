96a6f2ff00841c7e75ad0166d34b208fcc9bcaa6
93Kamuran <enginkamuran@gmail.com>
2020-10-26 15:49:46 +0300

    www-data user checking added to owner-changer.sh

--- Docker/owner-changer.sh
+++ Docker/owner-changer.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # Collect static files
-echo "Volume owner changing"
-chown -R www-data:www-data /var/www/html/mpmanager
-echo "Volume owner changed"
+
+if id "$1" >/dev/null 2>&1; then
+        echo "www-data user exist."
+        echo "changing volume owner.."
+        chown -R www-data:www-data /var/www/html/mpmanager
+        echo "Volume owner changed to www-data."
+else
+         echo "www-data user does not exist."
+         echo "Volume owner could not be changed, please adjust your permission setting for web server."
+fi
+
+
 php-fpm
