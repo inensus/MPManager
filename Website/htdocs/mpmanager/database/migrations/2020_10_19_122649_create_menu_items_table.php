cf18f1384ed4d9e5247b41c875e40a0485393c4d
alperburhan <ab@inensus.com>
2020-10-28 13:33:34 +0300

    Database Migrations & Seeder are Created

---
+++ Website/htdocs/mpmanager/database/migrations/2020_10_19_122649_create_menu_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('name',50);
+            $table->char('url_slug',50);
+            $table->char('md_icon',50);
+            $table->unsignedInteger('menu_order')->default(999);
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+
+
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+}
---
+++ Website/htdocs/mpmanager/database/seeds/SubMenuItemsSeeder.php
@@ -0,0 +1,116 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubMenuItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('sub_menu_items')->insert(array(
+                [
+                    'name' => 'Clusters',
+                    'url_slug' => '/',
+                    'parent_id' => '1',
+
+                ],
+                [
+                    'name' => 'Mini-Grid',
+                    'url_slug' => '/dashboards/mini-grid',
+                    'parent_id' => '1',
+
+                ],
+                [
+                    'name' => 'List',
+                    'url_slug' => '/agents/page/1',
+                    'parent_id' => '3',
+
+                ],
+                [
+                    'name' => 'Commission Types',
+                    'url_slug' => '/commissions',
+                    'parent_id' => '3',
+
+                ],
+                [
+                    'name' => 'List',
+                    'url_slug' => '/meters/page/1',
+                    'parent_id' => '4',
+
+                ],
+                [
+                    'name' => 'Types',
+                    'url_slug' => '/meters/types',
+                    'parent_id' => '4',
+
+                ],
+                [
+                    'name' => 'List',
+                    'url_slug' => '/tickets',
+                    'parent_id' => '6',
+
+                ],
+                [
+                    'name' => 'Users',
+                    'url_slug' => '/tickets/settings/users',
+                    'parent_id' => '6',
+
+                ],
+                [
+                    'name' => 'Categories',
+                    'url_slug' => '/tickets/settings/categories',
+                    'parent_id' => '6',
+
+                ],
+                [
+                    'name' => 'Groups',
+                    'url_slug' => '/connection-groups',
+                    'parent_id' => '10',
+
+                ],
+                [
+                    'name' => 'Types',
+                    'url_slug' => '/connection-types',
+                    'parent_id' => '10',
+
+                ],
+                [
+                    'name' => 'Sms List',
+                    'url_slug' => '/sms/list',
+                    'parent_id' => '11',
+
+                ],
+                [
+                    'name' => 'New Sms',
+                    'url_slug' => '/sms/newsms',
+                    'parent_id' => '11',
+
+                ],
+                [
+                    'name' => 'Add Cluster',
+                    'url_slug' => '/locations/add-cluster',
+                    'parent_id' => '14',
+
+                ],
+                [
+                    'name' => 'Add MiniGrid',
+                    'url_slug' => '/locations/add-mini-grid',
+                    'parent_id' => '14',
+
+                ],
+                [
+                    'name' => 'Add Village',
+                    'url_slug' => '/locations/add-village',
+                    'parent_id' => '14',
+
+                ],
+
+
+            )
+        );
+    }
+}
