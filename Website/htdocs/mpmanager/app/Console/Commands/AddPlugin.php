f8a441a3175c02db71a60a478e78d5495ee9ef96
alperburhan <ab@inensus.com>
2020-10-29 20:12:04 +0300

    Plugins model, table and command are created

---
+++ Website/htdocs/mpmanager/app/Console/Commands/AddPlugin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\PluginsService;
+use Illuminate\Console\Command;
+
+class AddPlugin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'plugin:add {name} {composer_name} {description}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Plugin Details adding to database';
+    /**
+     * @var PluginsService
+     */
+    private $pluginService;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param PluginsService $pluginsService
+     */
+    public function __construct(PluginsService $pluginsService)
+    {
+        parent::__construct();
+        $this->pluginService = $pluginsService;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $composer_name = $this->argument('composer_name');
+        $description = $this->argument('description');
+
+        $this->pluginService->addPlugin($name,$composer_name,$description);
+
+    }
+}
