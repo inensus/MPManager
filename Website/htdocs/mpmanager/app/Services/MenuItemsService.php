ed244eb7266a62a1b6367abdded0952305b3e5e7
93Kamuran <enginkamuran@gmail.com>
2020-10-30 18:07:34 +0300

    MenuItemsService.php refactored.

--- Website/htdocs/mpmanager/app/Services/MenuItemsService.php
+++ Website/htdocs/mpmanager/app/Services/MenuItemsService.php
@@ -3,8 +3,7 @@
 
 namespace App\Services;
 
-
-use App\MenuItems;
+use App\Models\MenuItems;
 use App\Models\SubMenuItems;
 
 class MenuItemsService
@@ -16,17 +15,19 @@ class MenuItemsService
 
     public function createMenuItems($menuItem, $subMenuItems)
     {
+
+        $lastOrder = MenuItems::query()->latest()->first();
         $menuItem = MenuItems::create([
-            'name'=>$menuItem->name,
-            'url_slug'=>$menuItem->url_slug,
-            'md_icon'=>$menuItem->md_icon,
-            'menu_order'=>$menuItem->menu_order,
+            'name'=>$menuItem['name'],
+            'url_slug'=>$menuItem['url_slug'],
+            'md_icon'=>$menuItem['md_icon'],
+            'menu_order'=>$lastOrder->menu_order+1,
         ]);
 
         foreach ($subMenuItems as $key=>$value){
             SubMenuItems::create([
-               'name'=>$value->name,
-               'url_slug'=>$value->url_slug,
+               'name'=>$value['name'],
+               'url_slug'=>$value['url_slug'],
                'parent_id'=>$menuItem->id
             ]);
 
