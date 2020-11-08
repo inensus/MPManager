b2b03e2f478c713f4ad44b179762362cc4708220
93Kamuran <enginkamuran@gmail.com>
2020-11-09 01:41:06 +0300

    Airtel and Vodacom logos have added to project.

--- Website/htdocs/mpmanager/resources/assets/js/components/Transactions/AirtelTransactionDetail.vue
+++ Website/htdocs/mpmanager/resources/assets/js/components/Transactions/AirtelTransactionDetail.vue
@@ -1,64 +1,73 @@
 <template>
-     <div>
+    <div>
 
 
-    <div class="md-layout">
-        <div class="md-layout-item md-subheader">
-            Mobile Provider
+        <div class="md-layout">
+            <div class="md-layout-item md-subheader">
+                Mobile Provider
+            </div>
+            <div class="md-layout-item md-subheader n-font">
+                <img class="logo" alt="logo" :src="imgLogo" style="max-height: 35px;"/>
+
+            </div>
         </div>
-        <div class="md-layout-item md-subheader n-font" >
-            <img src="https://assets.airtel.in/static-assets/selfcare/images/airtel-logo_opt.webp"
-             style="max-height: 35px;">
-        </div>
-    </div>
         <hr class="hr-d">
-    <div class="md-layout">
-        <div class="md-layout-item md-subheader">
-            Transaction ID
+        <div class="md-layout">
+            <div class="md-layout-item md-subheader">
+                Transaction ID
+            </div>
+            <div class="md-layout-item md-subheader n-font">
+                <span>{{ot.tr_id}}</span>
+            </div>
         </div>
-        <div class="md-layout-item md-subheader n-font">
-           <span>{{ot.tr_id}}</span>
-        </div>
-    </div>
         <hr class="hr-d">
-    <div class="md-layout">
-        <div class="md-layout-item md-subheader">
-            Transaction Identifier
+        <div class="md-layout">
+            <div class="md-layout-item md-subheader">
+                Transaction Identifier
+            </div>
+            <div class="md-layout-item md-subheader n-font">
+                {{ot.trans_id}}
+            </div>
         </div>
-        <div class="md-layout-item md-subheader n-font">
-            {{ot.trans_id}}
-        </div>
-    </div>
         <hr class="hr-d">
-    <div class="md-layout ">
-        <div class="md-layout-item md-subheader">
-            Business Number
-        </div>
-        <div class="md-layout-item md-subheader n-font">
-            {{ot.business_number}}
+        <div class="md-layout ">
+            <div class="md-layout-item md-subheader">
+                Business Number
+            </div>
+            <div class="md-layout-item md-subheader n-font">
+                {{ot.business_number}}
+            </div>
         </div>
     </div>
-     </div>
 
 </template>
 
 <script>
+import airtelLogo from '../../../../../storage/app/public/icons/airtel.png'
+
 export default {
     name: 'AirtelTransactionDetail',
-    props: ['ot']
+    props: ['ot'],
+    data () {
+        return {
+            imgLogo: airtelLogo
+
+        }
+    }
 }
 </script>
 
 <style scoped>
-    .n-font{
-        font-weight: 100!important;
+    .n-font {
+        font-weight: 100 !important;
     }
-    .hr-d{
+
+    .hr-d {
         height: 1pt;
         margin: auto;
         padding: 0;
         display: block;
         border: 0;
-        background-color: rgba(0,0,0,0.12);
+        background-color: rgba(0, 0, 0, 0.12);
     }
 </style>
