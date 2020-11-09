ed557da91342bc09e2c96c9c841894626100d810
Kemal <alchalade@gmail.com>
2020-11-09 22:35:44 +0300

    controller / http endpoint for generation asset readings

---
+++ Website/htdocs/mpmanager/app/Http/Controllers/GenerationAssetsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+use App\Services\GenerationAssetsService;
+
+class GenerationAssetsController extends Controller
+{
+    /**
+     * @var GenerationAssetsService
+     */
+    private $generationAssetsService;
+
+    /**
+     * GenerationAssetsController constructor.
+     * @param GenerationAssetsService $generationAssetsService
+     */
+    public function __construct(GenerationAssetsService $generationAssetsService)
+    {
+        $this->generationAssetsService = $generationAssetsService;
+    }
+
+    public function show($miniGridId): ApiResource
+    {
+        return new ApiResource(array_values($this->generationAssetsService->getGenerationAssets($miniGridId)));
+    }
+}
