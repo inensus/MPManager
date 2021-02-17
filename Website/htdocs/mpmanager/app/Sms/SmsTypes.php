5ec4758852f96f9727d7b1009032decf7c7fb7a5
kamuran engin <43153928+93Kamuran@users.noreply.github.com>
2021-02-17 22:45:42 +0300

    [BugFix] Generating Sms Body

--- Website/htdocs/mpmanager/app/Sms/SmsTypes.php
+++ Website/htdocs/mpmanager/app/Sms/SmsTypes.php
@@ -24,7 +24,7 @@ class SmsTypes
     public const ASSET_RATE_OVER_DUE_REMINDER = 4;
 
 
-    public static function generateSmsBody(TransactionDataContainer $data): string
+    public static function generateSmsBody($data): string
     {
         if ($data instanceof Transaction) {
             $payments = $data->paymentHistories()->get();
@@ -79,7 +79,8 @@ class SmsTypes
 
     private static function generateAssetRatePayment(PaymentHistory $paymentHistory): string
     {
-        $asset = $paymentHistory->paidFor()->first()->asset()->first();
+
+        $asset = $paymentHistory->paidFor->assetPerson->assetType;
         return 'Appliance: ' . $asset->name . ' Tshs ' . $paymentHistory->amount . '. ';
     }
 
