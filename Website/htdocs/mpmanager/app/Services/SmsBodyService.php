9089555358efd6ed627c6272bf4731c91c202d2f
93Kamuran <enginkamuran@gmail.com>
2021-03-15 12:56:53 +0300

    getSmsBodyByReference function refactored

--- Website/htdocs/mpmanager/app/Services/SmsBodyService.php
+++ Website/htdocs/mpmanager/app/Services/SmsBodyService.php
@@ -15,7 +15,7 @@ class SmsBodyService
 
     public function getSmsBodyByReference($reference)
     {
-        return  $this->smsBody->newQuery()->where('reference', $reference)->first();
+        return  $this->smsBody->newQuery()->where('reference', $reference)->firstOrFail();
     }
 
     public function getSmsBodies()
