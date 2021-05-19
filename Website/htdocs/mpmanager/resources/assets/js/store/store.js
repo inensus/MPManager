98ebfd4f3663b0b7e02c88790117410f537c1098
93Kamuran <enginkamuran@gmail.com>
2021-05-19 17:30:43 +0300

    admin class removed from store.js.

--- Website/htdocs/mpmanager/resources/assets/js/store/store.js
+++ Website/htdocs/mpmanager/resources/assets/js/store/store.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { Person } from '../classes/person'
 import { Meters } from '../classes/person/meters'
-import { Admin } from '../classes/admin'
 import * as auth from '../store/modules/authentication'
 import * as settings from '../store/modules/settings'
 import * as resolution from '../store/modules/resolution'
@@ -38,13 +37,11 @@ export default new Vuex.Store({
     state: {
         person: new Person(),
         meters: new Meters(),
-        admin: new Admin(),
         search: {},
     },
     getters: {
         person: state => state.person,
         meters: state => state.meters,
-        admin: state => state.admin,
         search: state => state.search,
         resolution: state => state.resolution
     }
