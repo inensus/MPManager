7dfbdbf4e0f5f5399aa67e37ed4a13c69714eca4
alperburhan <ab@inensus.com>
2021-05-30 14:48:59 +0300

    provider and bing map api key properties are added

--- Website/htdocs/mpmanager/database/seeds/MapSettingsSeeder.php
+++ Website/htdocs/mpmanager/database/seeds/MapSettingsSeeder.php
@@ -17,6 +17,8 @@ class MapSettingsSeeder extends Seeder
             'zoom' => 7,
             'latitude' => -2.500380,
             'longitude' => 32.889060,
+            'provider' => 'Open Street Map',
+            'bingMapApiKey' => '---',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
