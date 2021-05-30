e0e1fcc4fa100bb0a5211f49602a6b4762cc11a2
Kemal <alchalade@gmail.com>
2021-05-30 22:11:03 +0300

    nit, unnecessary nesting

--- Website/htdocs/mpmanager/resources/assets/js/services/MapSettingsService.js
+++ Website/htdocs/mpmanager/resources/assets/js/services/MapSettingsService.js
@@ -48,9 +48,9 @@ export class MapSettingsService {
 
     async checkBingMapApiKey () {
         try {
-            const data  = await this.repository.checkBingApiKey(
+            const { data } = await this.repository.checkBingApiKey(
                 this.mapSettings.bingMapApiKey)
-            return data.data.data.authentication
+            return data.data.authentication
         } catch (error) {
             return new ErrorHandler(error.response.data.message, 'http')
         }
