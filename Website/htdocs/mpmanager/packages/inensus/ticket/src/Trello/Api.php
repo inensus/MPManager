1325531e56607926abc9fe1d6309abc3e1dfe17c
alperburhan <ab@inensus.com>
2021-08-05 13:22:32 +0300

    [HotFix] changed build_query with Query::build()

--- Website/htdocs/mpmanager/packages/inensus/ticket/src/Trello/Api.php
+++ Website/htdocs/mpmanager/packages/inensus/ticket/src/Trello/Api.php
@@ -5,14 +5,11 @@
  * Date: 20.08.18
  * Time: 10:07
  */
-
 namespace Inensus\Ticket\Trello;
 
-
 use GuzzleHttp\Client;
-use function GuzzleHttp\Psr7\build_query;
+use GuzzleHttp\Psr7\Query;
 use Psr\Http\Message\ResponseInterface;
-
 
 class Api
 {
@@ -20,19 +17,15 @@ class Api
     public const POST = 1;
     public const DELETE = 2;
     public const PUT = 3;
-
     const APITOKEN = '9818ff631bd5a6cffa9d224675e1eb77de7df01f3ccaa92776d7d663a25c3c8f';
     const APIKEY = '0d9d80dbc26ccf55b5c605d910d9f27c';
     const APIURL = 'https://api.trello.com/1';
-
     private $httpClient;
-
 
     public function __construct(Client $httpClient)
     {
         $this->httpClient = $httpClient;
     }
-
 
     public function request(
         string $resource,
@@ -47,7 +40,7 @@ class Api
         $params['token'] = self::APITOKEN;
         switch ($type) {
             case self::GET:
-                $url .= '?' . build_query($params);
+                $url .= '?' . Query::build($params);
                 $request = $this->httpClient->get($url, $options);
                 break;
             case self::POST:
@@ -66,7 +59,7 @@ class Api
                 ]);
                 break;
         }
-
         return $request;
     }
 }
+
